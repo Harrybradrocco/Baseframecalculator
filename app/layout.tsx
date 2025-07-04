64fccd9b24f00faf05b7543be64a8eb36630edef
Harrybradrocco <Harrybradrocco@gmail.com>
2025-07-03 23:41:28 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Load Calculator',
+  description: 'Created with Next.js',
+  generator: 'hbradroc@uwo.ca',
 }
 
 export default function RootLayout({
